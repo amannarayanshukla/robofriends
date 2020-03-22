a18b0db4e79162b4fea0ec9ded9fc7aaa0797702
aman <aman@yellowmessenger.com>
2020-03-22 11:35:54 +0530

    Updates

---
+++ precache-manifest.472dc7a53db1124b18ef26d8de31b3c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b7b8d3d269127f8f97eeb68e2f61e94",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "ab99c822ba4ca86344f2",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "932b964a599aeb4c388f",
+    "url": "/robofriends/static/css/main.be500ee1.chunk.css"
+  },
+  {
+    "revision": "ab99c822ba4ca86344f2",
+    "url": "/robofriends/static/js/2.be32f29e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.be32f29e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "932b964a599aeb4c388f",
+    "url": "/robofriends/static/js/main.3ac486b2.chunk.js"
+  },
+  {
+    "revision": "4f2101bd33b0a3866fb5",
+    "url": "/robofriends/static/js/runtime-main.cf63ffc1.js"
+  }
+]);
